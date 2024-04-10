2550abef5958e192dfa97bf1b7299cc3cdd1822f
CriticalTim <105226272+CriticalTim@users.noreply.github.com>
2024-04-10 09:05:42 +0200

    letzte

---
+++ LogisticApp/Data/Migrations/20240313085042_blablab.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogisticApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class blablab : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
